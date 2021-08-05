071cc369d6d31bdf14ef96c09bdf568eb22f79e4
LucasGazetta <gazetinhagames@gmail.com>
2021-08-05 14:39:25 -0300

    busca na steam, microsoft store e EGS atualizadas

--- WebScrap.py
+++ WebScrap.py
@@ -6,25 +6,36 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
-import json
+import openpyxl
 
-#URL da steam
-url = "https://store.steampowered.com/app/753640/Outer_Wilds/"
+#URLs
+steam = "https://store.steampowered.com/app/753640/Outer_Wilds/"
+microsoft = "https://www.microsoft.com/pt-br/p/outer-wilds/c596fkdkmqn7?activetab=pivot:overviewtab"
+egs = "https://www.epicgames.com/store/pt-BR/p/outerwilds"
+
 
 option = Options()
 option.headless = True
 driver = webdriver.Firefox(executable_path=r'C:\Users\gazet\AppData\Local\Programs\Python\Python39\geckodriver.exe')
 
-
-driver.get(url)
-time.sleep(10)
+#Steam
+driver.get(steam)
 element = driver.find_element_by_xpath("//div[@class='game_purchase_price price']")
-html_content = element.get_attribute('innerHTML')
+html_content1 = element.get_attribute('innerHTML')
+print(html_content1)
 
-print(html_content)
+#Microsoft
+driver.get(microsoft)
+element = driver.find_element_by_xpath("//div[@class='pi-price-text']/.//span")
+html_content2 = element.get_attribute('innerHTML')
+print(html_content2)
 
-#Parseando o conteúdo HTML - BS
+driver.get(egs)
+element = driver.find_element_by_xpath("//div[@class='css-nxq7ez-PriceLayout__rowItem']/.//span")
+html_content3 = element.get_attribute('innerHTML')
+print(html_content3)
 
 
 
+time.sleep(10)
 driver.quit()
